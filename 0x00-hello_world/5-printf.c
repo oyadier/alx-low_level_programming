d1ce9c1a3a78c0e614ed155e712afc1a6b90ac79
root <oyadierrobertmensah@gmail.com>
2023-03-18 15:51:09 +0000

    Printing text using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - printing a text using printf function
+ *
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
-
