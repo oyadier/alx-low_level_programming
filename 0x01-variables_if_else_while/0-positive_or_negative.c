91a36971e75fcd5104d430a493b52201705c6b95
root <root@Oyatech.localdomain>
2023-03-19 16:16:28 +0000

    First commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ *  main: this generate random numbers
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%n is zero", n);
+	}
+	else 
+	{ 
+		print("%n is negative", n);
+	}
+	return (0);
+}
