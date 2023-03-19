5525642b7e8c2e953728d00f29dd9ef2bf0f92f2
root <oyadierrobertmensah@gmail.com>
2023-03-19 21:18:24 +0000

    Printing single digit only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - contain list of single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _numbers = 48;
+
+	while (_numbers < 58)
+	{
+		putchar(_number);
+		_numbers++;
+	}
+	putchar(10);
+	return (0);
+}
