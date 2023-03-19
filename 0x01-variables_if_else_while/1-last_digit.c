af5fe1f59d9950166a134432255fc2268596e9f8
root <oyadierrobertmensah@gmail.com>
2023-03-19 17:42:12 +0000

    First Commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 
 	if (last_num > 5)
 	{
-		printf("Last digit of %d is %d and is less than 5 and not 0", n, last_num);
+		printf("Last digit of %d is %d and is greater than 5 and not 0", n, last_num);
 	}
 	else if (last_num == 0)
 	{
