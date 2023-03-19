ddcd3b4a57c6d7abee9391db23c2e826dd712a15
root <oyadierrobertmensah@gmail.com>
2023-03-19 22:12:18 +0000

    Printing single digit using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - contains single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int _s_digit = 48;
+
+	while (_s_digit < 58)
+	{
+		putchar(_s_digit);
+		_s_digit++;
+	}
+	putchar(10);
+	return (0);
+}
