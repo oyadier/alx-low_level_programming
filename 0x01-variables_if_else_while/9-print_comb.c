a3330992821fe94ac1bbecd157bd4de1680e64f2
root <oyadierrobertmensah@gmail.com>
2023-03-20 00:55:39 +0000

    number combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,10 @@ int main(void)
 
 	while (_digit < 58)
 	{
-		if (_digit == 57)
-		{
-			putchar(_digit);
-		}
-		else
-		{
-			putchar(_digit);
-			putchar(44);
-			putchar(32);
-			_digit++;
-		}
+		putchar(_digit);
+		putchar(44);
+		putchar(32);
+		_digit++;
 	}
 	putchar(10);
 	return (0);
