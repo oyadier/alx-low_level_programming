da8b922c53f02a4b0ee63bf04992a1e964d89acc
root <oyadierrobertmensah@gmail.com>
2023-03-21 13:18:23 +0000

    First Commit:C project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a c function programming
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Writing c function project
