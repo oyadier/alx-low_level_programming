d0c8a2b542e84c5816b09febdf54cf56252a018c
root <oyadierrobertmensah@gmail.com>
2023-03-22 04:41:59 +0000

    Fixing first commit bug, betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,13 @@
  * 
  * Return: always zero (success)
  */
-
 void print_alphabet(void)
-	{
-		char _alpha;
+{
+	char _alpha;
 
-		for (_alpha = 'a'; _alpha <= 'z'; _alpha++)
+	for (_alpha = 'a'; _alpha <= 'z'; _alpha++)
 		{
 			_putchar(_alpha);
 		}
-		_putchar('\n');
-	}
+	_putchar('\n');
+}
