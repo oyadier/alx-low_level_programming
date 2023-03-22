e22f0e219eb90ccca6e161c31f8096e507722bab
root <oyadierrobertmensah@gmail.com>
2023-03-22 10:00:33 +0000

    printing the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - printing the last digit of an input
+ * @_num: the digit to be entered
+ * Return: Alway 0 success
+ */
+int print_last_digit(int _num)
+{
+	return (_num % 10);
+}
