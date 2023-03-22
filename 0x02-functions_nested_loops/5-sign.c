240418c0d9e716f87f6af74276d3d6f29855d2ee
root <oyadierrobertmensah@gmail.com>
2023-03-22 05:57:30 +0000

    print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - this print number is input is greater or less than zero
+ * @n: the argument
+ * Return: Always 0 success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
