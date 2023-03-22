d35d6140939100b510611f32dd8b4173aab6a819
root <oyadierrobertmensah@gmail.com>
2023-03-22 05:02:28 +0000

    fixing bug in task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * 1-alphabet.c - print alpharbet in lowercase
- * 
+ * print_alphabet - print alpharbet in lowercase
  * Return: always zero (success)
  */
 void print_alphabet(void)
@@ -9,8 +8,8 @@ void print_alphabet(void)
 	char _alpha;
 
 	for (_alpha = 'a'; _alpha <= 'z'; _alpha++)
-		{
-			_putchar(_alpha);
-		}
+	{
+		_putchar(_alpha);
+	}
 	_putchar('\n');
 }
