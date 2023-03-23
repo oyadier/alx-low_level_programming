cd7ae71e2407fa7bbb40e18fff1309c3731c5a9f
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-03-23 18:32:04 +0000

    Add two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - this multiply two integers
+ * @_inte1: first integer to be entered
+ * @_inte2: second integer to be entered
+ * Return: the result of the multiplied number
+ */
+int mul(int _inte1, int _inte2)
+{
+	return (_inte1 * _inte2);
+}
