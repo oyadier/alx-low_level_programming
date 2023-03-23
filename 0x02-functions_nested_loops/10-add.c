f54b14435cd2d9e16910ec384dd720822a503e0b
root <oyadierrobertmensah@gmail.com>
2023-03-23 04:02:39 +0000

    Add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two number and return result
+ * @_first: first number to be added
+ * @_second: second number to be added
+ * Return: returns the result
+ * */
+int add(int _first, int _second)
+{
+	return (_first + _second);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * jact_bauer - print the time of the day
+ * Return alway 0 success
+ */
+void jact_bauer(void)
+{
+	int ho;
+	int hi;
+	int mo;
+	int mi;
+	int minute;
+
+	for (minute = 0; minute <60; minute++)
+	{
+		
