b2b990a017cddde04145246458335ee326af4bd9
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-03-23 18:16:29 +0000

    It's a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - return 1 if it's digit else 0
+ * @chara: the character to be entered
+ * Return: 1 means is digit
+ */
+int _isdigit(int chara)
+{
+	return ((chara >= 48) && (chara <= 57));
+}
