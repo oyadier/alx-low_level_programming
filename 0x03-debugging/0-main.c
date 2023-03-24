7c1194110b2ea4f994f3cd299474aa6633e1ab1b
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-03-24 13:39:59 +0000

    Fixing betting Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,10 +4,13 @@
 * main - tests function that prints if integer is positive or negative
 * Return: always 0 succuss
 */
+
 int main(void)
 {
 	int i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
+
 }
