d275c249c791be546ab6d9f625ed4bcef87f48a7
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-03-24 04:24:41 +0000

    Fizz Buzz printing

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * fizz_buzz - it print fizz_buzz
+ * Return: Always 0 success
+ */
+void fizz_buzz(void)
+{
+	int counter;
+
+	for (counter = 1; counter <= 100; counter++)
+	{
+		if (((counter % 3) == 0) && ((counter % 5) != 0))
+		{
+			printf("Fizz ");
+		}
+		else if (((counter % 5) == 0) && ((counter % 3) != 0))
+		{
+			printf("Buzz ");
+		}
+		else if (((counter % 3) == 0) && ((counter % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+			printf("%d ", counter);
+	}
+}
