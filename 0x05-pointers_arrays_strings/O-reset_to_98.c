86ab5e80951d19bc3a5e93780f61dd60e199823c
Oyadier <oyadierrobertmensah@gmail.com>
2023-03-27 16:13:48 -0700

    First Commit

---
+++ 0x05-pointers_arrays_strings/O-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - change the value of pointer
+* variable of int
+* @*n: pointer variable of int
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+C programming of pointers
