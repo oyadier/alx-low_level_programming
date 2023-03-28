107270580ffa00d6207c67b82eb5acd1cd8d5614
Oyadier <oyadierrobertmensah@gmail.com>
2023-03-27 20:09:07 -0700

    Fix betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,10 @@
 /**
 * reset_to_98 - change the value of pointer
 * variable of int
-* @*n: pointer variable of int
+* @n: pointer variable of int
 * Return: nothing
 */
 void reset_to_98(int *n)
 {
-	*n = 98;
+*n = 98;
 }
