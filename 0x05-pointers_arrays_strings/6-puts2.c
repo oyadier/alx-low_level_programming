edf5141320565071a0f197c12b0c4657943ee1ab
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-03-30 14:31:08 +0100

    Print anyother character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - print any other character start with first one
+ * @str: the array of characters
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+
+	int length = 0;
+	int loop;
+
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+
+
+
+	for (loop = 0; loop <= length; loop++)
+	{
+		if ((str[loop] % 2) == 0)
+		{
+			_putchar(str[loop]);
+		}
+
+	}
+	_putchar('\n');
+}
