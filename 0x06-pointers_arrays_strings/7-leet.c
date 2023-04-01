9029aa4ddcc540ce0641355815b994577220e0f5
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-01 03:12:44 +0100

    String encoded

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes a string into integer
+ * @c: the string pointer
+ * Return: the c value
+ */
+char *leet(char *c)
+{
+	int x;
+	int k;
+	char a1[] = "4433007711";
+	char a2[] = "aAeEoOtTlL";
+
+	for (x = 0; c[x] != '\0'; x++)
+	{
+		for (k = 0; k < 10; k++)
+		{
+			if (c[x] == a1[k])
+			{
+				c[x] = a2[k];
+			}
+		}
+	}
+	return (c);
+}
