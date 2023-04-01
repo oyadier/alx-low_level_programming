029b6c55a30effd06738095ed717733623185f78
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-01 19:30:55 +0100

    Print half a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * puts_half - print half a string to the screen.
+ * @str: the string pointer
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int half;
+
+	while (str[length] != 0)
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		half = length / 2;
+	}
+	else
+		half = (length - 1) / 2;
+
+	while (str[half] != '\0')
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
