99a131375bc54a5ba7ab5b1e590058df0a24d7fc
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-04 02:05:13 +0100

    First commit, task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+int _putchar (char c);
+char *_memset(char *s, char cons_byte, unsigned int byte_size);
+char *_memcpy(char *dest, char *src, unsigned int byte_size);
+char *_shrchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
