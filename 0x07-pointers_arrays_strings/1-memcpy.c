bc6ee15610ec6ace7f796483a320803ad88c9bfd
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-04 02:52:50 +0100

    Copy byte to new memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _memcpy - copy n byte from memory to another memory
+ * @dest: new memory location
+ * @src: the existing memory location
+ * @n: number of bytes (characters)
+ * Return: the new memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_p = dest;
+	char *src_p = src;
+	int i = 0;
+
+	while (n > 0 && src[n] != '\0')
+	{
+		dest_p[i] = src_p[i];
+		i++;
+		n--;
+	}
+	dest[n] = '\0';
+
+	return (dest);
+}
