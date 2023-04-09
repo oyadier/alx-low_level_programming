1b1f90d44dcb9c79f5bfaaf643316044e382ac07
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-09 06:41:49 +0100

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the number of arguments passed
+ * @argc: the number of parameters entered
+ * @argv: array of pointers of string
+ * Return: 0 (Successfuly completed)
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	
+	printf("%d", argc - 1);
+	printf("\n");
+
+
+	return (0);
+}
