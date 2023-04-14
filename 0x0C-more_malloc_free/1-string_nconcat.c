40eb0d41953ae6f8a3f268d7432174190ae1c245
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-14 07:36:48 +0100

    Fixt bug, string concate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (i = 0; i < n; i++)
 	{
-		concat[len++] = s2[i]; /*add the second string the first*/
+		concat[len + i] = s2[i]; /*add the second string the first*/
 	}
 
-	concat[len] = '\0';
+	concat[len + i] = '\0';
 	return (concat);
 }
