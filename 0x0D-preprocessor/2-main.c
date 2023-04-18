e76c750163af7e02235cab138ba82a33f622184a
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-18 06:44:59 +0100

    Print name of a File

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print the name of a current file
+ * Return: 0 (success);
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
