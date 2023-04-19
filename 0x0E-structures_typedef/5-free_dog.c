c59a34e0c9175d22c8b634d58194975c3e014312
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-19 17:35:11 +0100

    Free dog pointer

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - free the dog pointer
+ * @d: a pointer to dog instances
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d);
+	}
+}
