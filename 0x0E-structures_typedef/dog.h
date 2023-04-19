7957fd57a91d3647d622137421930582da73100a
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-19 03:24:54 +0100

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - an object (structure) of a dog
+ * @name: the name
+ * @age: how old it is
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+}
+#endif
