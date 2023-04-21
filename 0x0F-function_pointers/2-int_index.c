01ba33f5bae147da7ae4a9df158bbd0afdf3d3f2
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-21 02:52:05 +0100

    Search for integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - returns the index of found integer in an array
+ * @array: the pointer to array of integers
+ * @size: the size of the array
+ * @cmp: the function pointer
+ * Return: 0 if @size is <= 0 and  -1 if > size
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+
+	}
+	return (-1);
+}
