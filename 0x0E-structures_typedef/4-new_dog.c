d3a3e182e1b5c943e8d673b0d2e3a4f9f379f2aa
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-22 15:48:24 +0100

    Fix bug, new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,13 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * new_dog - Create dog instance
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: array of string pointer to the owner of the dog
  *
- * Returns: NULL is creating the new dog fails
+ * Return: new dog is creating the new dog fails
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
@@ -19,6 +18,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (_new_dog == NULL)
 		return (NULL);
+
+	if (name == NULL)
+	{
+		free(_new_dog);
+		free(owner);
+		return (NULL);
+	}
+	if (owner == NULL)
+	{
+		free(_new_dog);
+		free(name);
+		return (NULL);
+	}
 
 	_new_dog->name = name;
 	_new_dog->age = age;
