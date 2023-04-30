f6c75343cc499745cabf5e87df7f8ce59cfa960f
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-30 05:48:14 +0100

    Fix Bug in Add Node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,8 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
- * add_node_end - add a new node at the first
+ * lenght - calculate the lenght of a string
+ * @str: the pointer to string array
+ *
+ * Return: the lenght of the string
+ */
+
+unsigned int lenght(const char *str)
+{
+	unsigned int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_node - add a new node at the first
  * @head: a pointer to a pointer head
  * @str: pointer to string
  * Return: the address of the new element else NULL
@@ -15,8 +34,8 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = NULL;
-	
-if (str == NULL)
+
+	if (str == NULL)
 	{
 		return (NULL);
 	}
@@ -28,6 +47,7 @@ if (str == NULL)
 		return (NULL);
 	}
 	new->str = strdup(str);
+	new->len = lenght(str);
 	if (new->str == NULL)
 	{
 		free(new);
