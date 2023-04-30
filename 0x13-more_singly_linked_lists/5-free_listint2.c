0824566392345dadcee4fe99ce75eb2334b611be
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-30 22:40:28 +0100

    Free memory and set NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free up the space used by the listint
+ * @head: a pointer to the listint node
+ * Return: nothing
+ *
+ *Description: This function free up the space used by the
+ *		listint node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *current;
+
+	while (temp != NULL)
+	{
+
+		current = temp->next;
+		free(temp);
+		temp = current;
+
+	}
+	*head = NULL;
+}
