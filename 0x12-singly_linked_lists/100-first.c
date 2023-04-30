6b62226b1460cd26e4ad4f15424a2c98f6812bce
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-30 08:17:51 +0100

    Print a constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@
 
 void __attribute__((constructor)) welcome_message()
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
