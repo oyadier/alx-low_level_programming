70cacfaeccd1bf9b6220833a97c157d26fa1c605
Oyadier Robert Mensah <oyadierobertmensah@gmail.com>
2023-04-30 07:43:49 +0100

    Free space

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,12 +11,11 @@
 void free_list(list_t *head)
 {
 	list_t *temp = NULL;
-	list_t *_current = head;
 
-	while (_current != NULL)
+	while (head != NULL)
 	{
-		temp = _current;
-		_current = _current->next;
+		temp = head;
+		head = head->next;
 
 		free(temp->str);
 		free(temp);
