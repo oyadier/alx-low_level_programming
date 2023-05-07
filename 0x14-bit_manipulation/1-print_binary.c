06b1d8a74e2d20f0aaaf335e7abafe845bbb028e
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-05-07 05:43:28 +0100

    Fix bug

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,16 +12,25 @@ void print_binary(unsigned long int n)
 	unsigned long int bits_size = sizeof(unsigned long int) * 8;
 	unsigned long int pos;
 	unsigned long int i;
+	int leading_zero = 0;
+
 
 	pos = ((unsigned long int) 1) << (bits_size - 1);
 	for (i = 0; i < bits_size; ++i)
 	{
 		if (n & pos)
+		{
+			leading_zero = 1;
 			printf("1");
-		else
+		}
+		else if (leading_zero)
 			printf("0");
 
 		pos >>= 1;
 	}
+
+	if (!leading_zero)
+		printf("0");
+
 }
 
