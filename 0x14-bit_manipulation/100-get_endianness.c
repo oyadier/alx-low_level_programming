738dc838620228e004d26d0973042a66d2e86359
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-05-27 18:53:38 +0100

    Fix Bug

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,11 +7,12 @@
  */
 int get_endianness(void)
 {
-int num = 1;
-char *endian = (char *)&num;
-if (*endian == 1)
-{
-return (1);
-}
-return (0);
+	int num = 1;
+	char *endian = (char *)&num;
+
+	if (*endian == 1)
+	{
+		return (1);
+	}
+	return (0);
 }
