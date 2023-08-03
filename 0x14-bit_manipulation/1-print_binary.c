3fe65232e8a42f1d44d25601985525cad1f9bfd8
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-08-03 23:16:52 +0000

    Int to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: the integer number
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int bits_size = sizeof(unsigned long int) * 8;
+	unsigned long int pos;
+	unsigned long int i;
+	int leading_zero = 0;
+
+
+	pos = ((unsigned long int) 1) << (bits_size - 1);
+	for (i = 0; i < bits_size; ++i)
+	{
+
+		if (n & pos)
+		{
+			leading_zero = 1;
+			printf("1");
+		}
+		else if (leading_zero)
+			printf("0");
+
+		pos >>= 1;
+	}
+
+	if (!leading_zero)
+		printf("0");
+
+}
+
+
