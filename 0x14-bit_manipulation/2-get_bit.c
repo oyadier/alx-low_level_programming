b5caf4448a48d723ec6f12002714ec16f63b06b5
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-09-01 07:05:14 +0000

    Unneccessary type casting

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,12 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int bits_size = sizeof(unsigned long int) * 8;
-	unsigned long int pos;
+	unsigned long int x = 1, pos;
 	unsigned long int i;
 	int l_zero = 0;
 
 
-	pos = 1  << (bits_size - 1);
+	pos = x << (bits_size - 1);
 	for (i = 0; i < bits_size; ++i)
 	{
 		if (n & pos)
