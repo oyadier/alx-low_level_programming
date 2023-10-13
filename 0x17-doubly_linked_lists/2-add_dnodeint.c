a9a10c0d87d48b8b80db7d8e39f6cab33ec1f745
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-10-13 05:09:46 +0000

    Add new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,10 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
-	{
-		dprintf(2, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
+		return (NULL);
 
 	newNode->n = n;
 	newNode->next = *head;
