d38ed9134c7fbed7306ea685461d1fe6467656c3
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2023-11-04 04:26:28 +0000

    Key Index of hash table

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - compute the index of the array(hash_table)\
+ *		@key and @size (array size)
+ * @key: the string key to be used for hash_value
+ * @size: the size of the array or associative array
+ *
+ * Return: the array index to store the node value
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value, key_index;
+
+	hash_value = hash_djb2(key);
+	key_index = hash_value % size;
+
+	return (key_index);
