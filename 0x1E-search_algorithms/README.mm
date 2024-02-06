d1d54aedf5475f53aabf6787e21cc7a384436fdb
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-02-06 10:59:09 +0000

    First Commit

---
+++ 0x1E-search_algorithms/README.mm
@@ -0,0 +1,8 @@
+<h1>0x1E. C - Search Algorithms </h1>
+<p>
+	<h2> General Objective<h2>
+    What is a search algorithm
+    What is a linear search
+    What is a binary search
+    What is the best search algorithm to use depending on your needs
+ </p>
